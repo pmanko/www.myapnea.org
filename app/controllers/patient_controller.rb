303336e3a387434acc52163339b2f3fb8449cab2
Piotr Mankowski <Piotr Mankowski>
2014-03-25 20:04:06 -0400

    Work for presentation

---
+++ app/controllers/patient_controller.rb
@@ -0,0 +1,4 @@
+class PatientController < ApplicationController
+  def dashboard
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'patient/dashboard'
+
   resources :templates
 
   resources :questions
