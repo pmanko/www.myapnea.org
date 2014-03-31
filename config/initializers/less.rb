d15f3f369fe13a54a22df51954abdb61b3d1bcb4
Piotr Mankowski <Piotr Mankowski>
2014-03-31 18:00:31 -0400

    Theme switcher fixes

--- config/initializers/less.rb
+++ config/initializers/less.rb
@@ -1,7 +1,7 @@
 WwwMyapneaOrg::Application.configure do
-  config.less.paths << "#{Rails.root}/vendor/assets/bootstrap/less"
-  config.less.paths << "#{Rails.root}/lib/assets/bootstrap/less"
-  config.assets.paths << "#{Rails.root}/lib/assets/bootstrap/less"
-  config.assets.paths << "#{Rails.root}/vendor/assets/bootstrap/less"
+  #config.less.paths << "#{Rails.root}/vendor/assets/bootstrap/less"
+  #config.less.paths << "#{Rails.root}/lib/assets/bootstrap/less"
+  #config.assets.paths << "#{Rails.root}/lib/assets/bootstrap/less"
+  #config.assets.paths << "#{Rails.root}/vendor/assets/bootstrap/less"
   config.less.compress = true
 end
