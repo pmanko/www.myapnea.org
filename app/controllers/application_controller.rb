bdb56fea0c79bcd0120ccc052963aa2aa2480aa7
Piotr Mankowski <pmankowski@partners.org>
2014-04-08 15:51:01 -0400

    Template fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :first_name
     devise_parameter_sanitizer.for(:sign_up) << :last_name
   end
-  
+
   def after_sign_in_path_for(resource)
     dashboard_path
   end
