43b5ad46301c1f384471aa99009b45c6a08f5e38
Piotr Mankowski <pmankowski@partners.org>
2014-04-08 21:18:14 -0400

    Migration fix

--- db/migrate/20140325051533_create_question_flows.rb
+++ db/migrate/20140325051533_create_question_flows.rb
@@ -1,6 +1,6 @@
 class CreateQuestionFlows < ActiveRecord::Migration
   def change
-    create_table :question_nodes do |t|
+    create_table :question_flows do |t|
       t.integer :parent_id
       t.integer :answer_option_id
       t.string :condition
