cf8543fa277c14d2507d2ea26b4101291792b502
Piotr Mankowski <Piotr Mankowski>
2014-05-13 14:35:11 -0400

    Geocoder fix

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -9,7 +9,7 @@ class MailingListsController < ApplicationController
     ml.email = params[:email]
     ml.ip_address = request.remote_ip
 
-    if geocoder_result = request.location
+    if geocoder_result = Geocoder.search(request.remote_ip).first
       ml.zip_code = geocoder_result.postal_code
       ml.country = geocoder_result.country_code
       ml.state = geocoder_result.state_code
