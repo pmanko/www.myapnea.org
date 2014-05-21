ebb09fba2c81bcfe1045f555d72c67142f2aeb36
Piotr Mankowski <Piotr Mankowski>
2014-05-21 13:58:51 -0400

    locales

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module WwwMyapneaOrg
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
 
 
     # Precompile additional assets.
