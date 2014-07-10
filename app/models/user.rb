835ea3dc53ac3f125de79d4403c7ba87933069a6
Piotr Mankowski <Piotr Mankowski>
2014-07-10 13:12:17 -0400

    Password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable #:confirmable,, #:validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :answer_sessions
 
